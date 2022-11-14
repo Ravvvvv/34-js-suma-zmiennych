d18e5492dc376558c62f9748a5de8bf1b38ec059
rafal a <rafalandrzejczuk1@gmial.com>
2022-11-14 15:41:18 +0100

    zadanie

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 3
+let secondNumber = 4
+
+function multiply(num1, num2) {
+    let suma = num1 + num2;
+
+    return suma;
+}
+
+console.log(multiply(firstNumber,secondNumber));
+
